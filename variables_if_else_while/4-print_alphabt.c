2cc027735e77d7daca3e9a9ea4fd40e29f519125
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-15 23:08:36 +0000

    print abc except e and q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet, except q and e.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp[24] = "abcdfghijklmnoprstuvwxyz";
+int i;
+for (i = 0; i < 24; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
