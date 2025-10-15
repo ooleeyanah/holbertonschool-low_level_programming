9a7037e52e2c0485e9012f712f192119d412e3fa
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-15 22:25:49 +0000

    %d instead of %n

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,27 +3,26 @@
 #include <stdio.h>
 
 /**
- * main - Is the number positive, negative, or zero.
+ * main - Determines if a number is positive, negative, or zero.
  *
- * return: Always 0 (Success)
- */ 
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	  {
-	    printf("%d is positive\n", n);
-	  }
-	else if (n == 0)
-	  {
-	    printf("%n is zero\n", n);
-	  }
-	else
-	  {
-	    printf("%d is negative\n", n);
-	  }
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
 }
