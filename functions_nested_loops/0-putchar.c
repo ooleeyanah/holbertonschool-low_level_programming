53d3eb2f98cc8472ff0ecb8ed4d7c5fea7a63603
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-17 01:40:23 +0000

    prints putchar_ using putchar and string

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints message "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char str[] = "_putchar";
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
+return (0);
+}
