367273450b9667c68e4e1a3237ede0c935a930b7
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-20 06:29:54 +0000

    prints line based on input

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - prints a line based on input
+ * @n: times the line is printed
+ * Return: no return
+ */
+void print_line(int n);
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar(85);
+}
+_putchar('\n');
+}
