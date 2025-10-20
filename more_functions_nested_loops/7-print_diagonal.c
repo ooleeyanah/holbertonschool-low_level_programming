eb2af68b9497af88abc564a5b7ec86aa1ff771ee
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-20 06:38:22 +0000

    prints diagonal line based on input

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_diagonal - prints diagonal line based on input
+ * @n: times the line is printed
+ * Return: no return
+ */
+void print_diagonal(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar(92);
+}
+_putchar('\n');
+}
