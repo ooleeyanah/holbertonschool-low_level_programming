dbb8026521ecab6143965e3a4f0e7ab7922e3f14
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-20 00:27:28 +0000

    fixed typo

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - Determines if the input number
- * greater, iqual or less than zero.
+ * greater, equal or less than zero.
  *
  * @n: The input number as an integer.
  *
