bd485aaaa117b782db0a2650912f2b0d888cd35a
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-20 06:41:37 +0000

    add space

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,10 +6,16 @@
  */
 void print_diagonal(int n)
 {
-int i;
+int i, a;
 for (i = 0; i < n; i++)
 {
+for (a = 0; a < i; a++)
+{
+_putchar(' ');
+}
 _putchar(92);
+if (i < (n - 1))
+_putchar('\n');
 }
 _putchar('\n');
 }
