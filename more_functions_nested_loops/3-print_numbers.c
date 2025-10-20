3fd797c66dfaf9eab409a2c1511cef6b01741ac6
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-20 06:07:43 +0000

    print 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9 with new lines
+ *
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+int ch;
+for (c = 48; ch < 58; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
