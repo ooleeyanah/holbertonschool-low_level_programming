56d22a2cf4c078a7020d57112e306244e31fccba
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-21 23:57:12 +0000

    prints every second char in str

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character in string
+ * @str: string input
+ * Return: no return
+ */
+void puts2(char *str);
+{
+int count = 0;
+while (count >= 0)
+{
+if (str[count] == '\0')
+{
+_putchar('\n');
+break;
+}
+if (count % 2 == 0)
+{
+_putchar(str[count]);
+count++;
+}
+}
