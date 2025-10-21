395cb8b8baf040ab2bbd03bf8b63181b45da6993
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-21 03:14:47 +0000

    fiiix

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,14 +6,22 @@
  */
 void rev_string(char *s)
 {
-int count = 0;
+int count = 0, i, j;
+char *str, temp;
 while (count >= 0)
 {
 if (s[count] == '\0')
 break;
 count++;
 }
-for (count--; count >= 0; count--)
-_putchar(s);
-_putchar('\n');
+str = s;
+for (i = 0; i < (count - 1); i++)
+{
+for (j = i + 1; j > 0; j--)
+{
+temp = *(str + j);
+*(str + j) = *(str + (j - 1));
+*(str + (j - 1)) = temp;
+}
+}
 }
