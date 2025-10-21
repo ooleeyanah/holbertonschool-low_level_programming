86f7f820c918c0a90271637817f49daa9cea16f9
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-21 03:09:20 +0000

    reverse string without print

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * rev_string - reverses string
+ * @s: input string
+ * Return: no return
+ */
+void rev_string(char *s)
+{
+  int count = 0;
+  while (count >= 0)
+    {
+      if (s[count] == '\0')
+	break;
+      count++;
+    }
+}
