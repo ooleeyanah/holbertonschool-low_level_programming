9336bfd96ff6db4d36b76fb2d1039fca696c5ad3
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-21 01:40:50 +0000

    cal length of string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - calculates length of a string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+int count = 0;
+while (*(s + count) != '\0')
+count++;
+return (count);
+}
