d7109803f9468d4d651d9c02ac037416b970587f
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-21 01:29:02 +0000

    swaps ints

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps values of two integers
+ * @a: integer one
+ * @b: integer two
+ * Return: no return
+ */
+void swap_int(int *a, int *b)
+{
+int c = *a;
+*a = *b;
+*b = c;
+}
+
