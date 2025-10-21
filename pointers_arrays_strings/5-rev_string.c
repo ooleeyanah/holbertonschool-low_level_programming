451e121c8a76d097038129ee1b0bbc5daf241e87
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-21 03:11:49 +0000

    reverse string ggg

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,11 +6,14 @@
  */
 void rev_string(char *s)
 {
-  int count = 0;
-  while (count >= 0)
-    {
-      if (s[count] == '\0')
-	break;
-      count++;
-    }
+int count = 0;
+while (count >= 0)
+{
+if (s[count] == '\0')
+break;
+count++;
+}
+for (count--; count >= 0; count--)
+_putchar(s);
+_putchar('\n');
 }
