d2afdf06d2018b5a62e927889296b84f4d7df956
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-22 00:48:35 +0000

    fixed typos

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,17 +10,14 @@ int count = 0, i;
 while (count >= 0)
 {
 if (str[count] == '\0')
-{
-_putchar('\n');
 break;
 count++;
 }
-if (str[count] % 2 == 1)
+if (count % 2 == 1)
 i = count / 2;
 else
 i = (count - 1) / 2;
 for (i++; i < count; i++)
 _putchar(str[i]);
 _putchar('\n');
-}
 }
