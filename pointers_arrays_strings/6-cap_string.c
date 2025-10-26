4b2e5264a4bdaaca2e42998efaecd85d48d3e67c
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-26 06:21:20 +0000

    capitalize words

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *cap_string - capitalizes all words of a string
+ * @s: string
+ * Return: capitalized string
+ */
+char *cap_string(char *s)
+{
+int i = 0, j;
+char sep[] = " \t\n,;.!?\"(){}";
+while (s[i] != '\0')
+{
+if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+s[i] -= 32;
+for (j = 0; sep[j] != '\0'; j++)
+{
+if (s[i] == sep[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+{
+s[i + 1] -= 32;
+break;
+}
+}
+i++;
+}
+return (s);
+}
