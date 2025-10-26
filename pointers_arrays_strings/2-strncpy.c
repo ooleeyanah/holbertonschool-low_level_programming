dc5f4e81aed73ee4b6406b1571a996f37b3a48c1
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-26 03:46:01 +0000

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strncpy - copies string
+ * @dest: destination
+ * @src: source
+ * @n: number of chars
+ * Return: copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int count = 0;
+while (count >= 0)
+{
+*(dest + count) = *(src + count);
+if (*(src + count) == '\0')
+break;
+count++;
+}
+return (dest);
+}
