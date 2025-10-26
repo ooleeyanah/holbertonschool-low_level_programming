d77a06a30a80208d40f7da94ac5d2fcb78bea81a
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-26 04:10:56 +0000

    int i

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,6 +9,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int count = 0;
+int i = 0;
 while (count >= 0)
 {
 if (n <= count)
@@ -20,7 +21,7 @@ count++;
 }
 if (count == 0)
 {
-for (int i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
 *(dest + i) = '\0';
 }
