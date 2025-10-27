b80a01640648344be619ce8937f0feb8dd90fc0f
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-27 02:52:09 +0000

    unsigned

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -8,14 +8,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i;
-for (i = 0; i < n && src[i] != '\0'; i++)
+unsigned int i;
+for (i = 0; i < n; i++)
 {
 dest[i] = src[i];
-}
-for (; i < n; i++)
-{
-dest[i] = '\0';
 }
 return (dest);
 }
