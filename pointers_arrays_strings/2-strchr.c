6a4b5e24e5a1437d307547ecbefc770634b9f691
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-27 04:26:45 +0000

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - locates char in a string
+ * @s: string
+ * @c: desired char
+ * Return: pointer to first c occurence or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+return (&s[i]);
+}
+if (s[i] == c)
+return (&s[i]);
+return (NULL);
+}
