f328f5fbf7dfda9ea667bd34abfb1cc645a35d16
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-27 05:51:08 +0000

    fix

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -7,18 +7,14 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-unsigned int i, j, count;
-count = 0;
+unsigned int i, j;
 for (i = 0; s[i] != '\0'; i++)
 {
 for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
-break;
+return (&s[i]);
 }
-if (accept[j] == '\0')
-return (count);
-count++;
 }
-return (count);
+return (0);
 }
