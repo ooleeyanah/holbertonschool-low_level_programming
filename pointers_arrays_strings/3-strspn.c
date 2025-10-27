de0d6938209836d8da4160b09eb559f8aa48c0ae
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-27 05:08:36 +0000

    removed space before [

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,7 +11,7 @@ unsigned int i, j, count;
 count = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; accept [j] != '\0'; j++)
+for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
 break;
