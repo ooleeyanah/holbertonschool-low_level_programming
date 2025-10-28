0ed014627d08f8dc3e71cac2872863052eab474c
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-28 02:46:10 +0000

    no count

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -5,11 +5,8 @@
  * Return: length of string
  */
 int _strlen_recursion(char *s)
-{ int count = 0;
-if (*s != '\0')
 {
-_strlen_recursion(s + 1);
-count++;
-}
-return (count);
+if (*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
 }
