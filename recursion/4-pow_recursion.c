d4689377b801f4c8477e48e30cf8491b5c17a00f
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-28 03:54:55 +0000

    pow recursion

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns value of x to power of y
+ * @x: int one
+ * @y: int two
+ * Return: value of x to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (0);
+return (_pow_recursion(y *= x));
+}
