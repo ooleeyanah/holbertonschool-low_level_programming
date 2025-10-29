2cd812f958b1667fabe6e38ec36da1b8fca1bec1
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-29 05:10:33 +0000

    prints num of args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print num of arguments passed from command line
+ * @argc: num of command line arguments
+ * @argv: array with command line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argv; i++)
+printf("%s\n", argv[i]);
+return (0);
+}
