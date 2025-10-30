cec2e441c6c591dfa00618072c0c469464193cb2
Uliana Deshin <uliana.deshin@gmail.com>
2025-10-30 02:33:54 +0000

    mult two nums

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print num of arguments passed from command line
+ * @argc: num of command line arguments
+ * @argv: array with command line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
