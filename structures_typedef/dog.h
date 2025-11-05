62de6e7f88da53c00b429a90a1780435e75271e6
Uliana Deshin <uliana.deshin@gmail.com>
2025-11-05 05:32:06 +0000

    dog info structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - new struct for dog info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
