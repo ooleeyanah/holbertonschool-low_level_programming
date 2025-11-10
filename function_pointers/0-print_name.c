b660219277bbe9ee7f24d44f2341bffaf6b6de78
Uliana Deshin <uliana.deshin@gmail.com>
2025-11-10 22:26:00 +0000

    prints name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: the name
+ * @f: pointer to function
+ * Return: no return
+ */
+void print_name(char *name, void (*f)(char *))
+{
+printf("%s\n", name);
+}
