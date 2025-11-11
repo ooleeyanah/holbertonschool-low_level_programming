2d4c08eef1cc8ea01b21a2924c304bc6f9235ca3
Uliana Deshin <uliana.deshin@gmail.com>
2025-11-11 00:16:58 +0000

    selects correct function for input

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,25 @@
+#include "calc.h"
+/**
+ * get_op_func - selects function to perform arithmetic
+ * @s: operator as argument
+ * Return: pointer to function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
+while (i < 10)
+{
+if (s[0] == ops->op[i])
+break;
+i++;
+}
+return (ops[i / 2].f);
+}
