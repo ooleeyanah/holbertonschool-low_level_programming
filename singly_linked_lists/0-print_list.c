463c51392612b81f4556e1cd2af0298ef69ed609
Uliana Deshin <uliana.deshin@gmail.com>
2025-11-24 23:00:01 +0000

    betty style

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,17 +1,22 @@
 #include "lists.h"
 #include <stdio.h>
+/**
+ * print_list - prints list
+ * @h: head
+ * Return: count
+ */
+
 size_t print_list(const list_t *h)
 {
-  size_t count = 0;
-  while (h != NULL)
-    {
-      if (h->str == NULL)
-	printf("[0] (nil)\n");
-      else
-	printf("[%u] %s\n", h->len, h->str);
-      count++;
-      h = h->next;
-    }
-  return (count);
+size_t count = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+count++;
+h = h->next;
 }
-      
+return (count);
+}
