de58821825a2057856dc63495a114880d5fa23b5
Uliana Deshin <uliana.deshin@gmail.com>
2025-11-24 22:46:41 +0000

    struct list_s

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,12 @@
 #include <stddef.h>
 #ifndef _LISTS_H_
 #define _LISTS_H_
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 int _putchar(char c);
 size_t print_list(const list_t *h);
 #endif
