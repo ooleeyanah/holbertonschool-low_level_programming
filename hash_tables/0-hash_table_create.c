1111693a4fbc4a3963e2c50cca06d0257a38e952
Uliana Deshin <uliana.deshin@gmail.com>
2025-12-03 03:48:10 +0000

    creates table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+/**
+ * hash_table_create - creates hash table
+ * @size: size of array
+ * Return: pointer to new table or NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *ht;
+ht = malloc(sizeof(hash_table_t));
+if (!ht)
+return (NULL);
+ht->size = size;
+ht->array = calloc(size, sizeof(hash_node_t *));
+if (!ht->array)
+{
+free(ht);
+return (NULL);
+}
+return (ht);
+}
