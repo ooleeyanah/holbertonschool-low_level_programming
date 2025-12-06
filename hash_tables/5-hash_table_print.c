a9568c30af1f108b47a1cf6036a016d3a1b05277
Uliana Deshin <uliana.deshin@gmail.com>
2025-12-06 02:57:49 +0000

    prints hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints hash table
+ * @ht: hash table
+ * Return: no return
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned long int i;
+hash_node_t *node;
+int first = 1;
+if (!ht)
+return;
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+node = ht->array[i];
+while (node)
+{
+if (!first)
+printf(", ");
+printf("'%s': '%s'", node->key, node->value);
+first = 0;
+node = node->next;
+}
+}
+printf("}\n");
+}
